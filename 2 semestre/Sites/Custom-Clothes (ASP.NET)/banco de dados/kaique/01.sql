7c189dd1f0772d5b7f4230bfa9b5e58a6d051159
KaiqueVfreitas <kaique.vieira2502eu@outlook.com>
2024-06-04 15:38:12 -0300

    Adicionei um arquivo txt que está as tabelas que preciso criar

--- 2 semestre/Sites/Custom-Clothes (ASP.NET)/banco de dados/kaique/01.sql
+++ 2 semestre/Sites/Custom-Clothes (ASP.NET)/banco de dados/kaique/01.sql
@@ -4,3 +4,15 @@
 create database dbCustomClothesAspNet;
 use dbCustomClothesAspNet;
 
+-- Tabela do login e cadastro de clientes
+create table tbLoginCliente(
+	nome varchar(50) not null,
+    email varchar(50) not null,
+    senha password(30) not null,
+    idCliente primary key autoincrement
+);
+
+-- Tabela do cadastro das compras do cliente
+tbCadCompra(
+	
+);
