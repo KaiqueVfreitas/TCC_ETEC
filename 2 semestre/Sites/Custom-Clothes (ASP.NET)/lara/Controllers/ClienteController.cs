df88977e08126fee2ee675fe06f2cdc7c66b840d
LARA PAIVA <lara.paiva@etec.sp.gov.br>
2024-06-04 21:23:16 -0300

    adicionei minha tela de cadastro a minha pasta

---
+++ 2 semestre/Sites/Custom-Clothes (ASP.NET)/lara/Controllers/ClienteController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomClothing.Controllers
+{
+
+    public class ClienteController : Controller
+    {
+        // ILogger permite retornar erros e avisos dos nossos sistemas de forma simples e fácil
+        private readonly ILogger<ClienteController> _logger;
+        
+
+    }
+}
---
+++ 2 semestre/Sites/Custom-Clothes (ASP.NET)/lara/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomClothing.Models
+{
+        public class Cliente
+        {//criando o encapsulamento DO OBJETO COM GET E SET
+            public int Id { get; set; }
+            public string? Nome { get; set; }       
+            public string? Email { get; set; }
+
+            public List<Cliente>? ListaCliente { get; set; }
+
+        }
+    
+
+}
+
